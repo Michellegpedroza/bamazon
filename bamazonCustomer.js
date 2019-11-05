6fced5271a7e790eb1a02739f08bc25f36edb7d2
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-11-05 11:13:43 -0800

    created prompts for the user to indicate which products they want to buy

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,49 @@
+// Required node packages
 const inquirer = require(`inquirer`)
-
 const mysql = require(`mysql2`)
 
-const db = mysql.createConnection ({
+//Connecting to your MySql Database
+const db = mysql.createConnection({
   host: 'localhost',
   user: `root`,
   password: `password`,
   database: `bamazon_db`
 })
 
+const displayProducts = () => {
+  //Display all Items In the Database
+  db.query(`SELECT * FROM products`, (e, data) => {
+    if (e) {
+      console.log(e)
+    }
+    console.log(data)
+  })
+}
+displayProducts()
+
+
+const promptCustomer = () => {
+  // Prompt user with two messages
+  inquirer
+    .prompt([
+      {
+        type: `number`,
+        message: `What is the item ID of the product you would like to buy?`,
+        name: `ID`
+      },
+      {
+        type: `number`,
+        message: `How many would you like to buy?`,
+        name: `Quantity`
+      }
+    ])
+    //Check if store has enough of the product to meet customers request.
+    .then(function (number) {
+      let item = number.ID
+      let quantity = number.Quantity
+
+      console.log(`Item ID: ${item}`)
+      console.log(`Quantity: ${quantity}`)
+    })
+}
+promptCustomer()
