5b48e06bbbfd604066ef7418be468281c0fec795
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-11-10 20:54:31 -0800

    code is working and running but receiving a mysql syntax error.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,13 +11,13 @@ const db = mysql.createConnection({
 })
 
 
-//Display all Items In the Database
-function displayProducts() {
-  let selectQuery = `SELECT * FROM products`
-  db.query(selectQuery, (e, data) => {
+function bamazon() {
+  //Select all items from the Database
+  db.query(`SELECT * FROM products`, (e, data) => {
     if (e) {
       console.log(e)
     }
+    //Display all Items In the Database
     console.log(`Products:`)
     let prodList = ``
     for (let i = 0; i < data.length; i++) {
@@ -29,6 +29,8 @@ function displayProducts() {
       console.log(prodList)
     }
     console.log(`___________________________________________________________________________________________`)
+
+    //prompt the customer with two questions
     inquirer
       .prompt([
         {
@@ -42,45 +44,42 @@ function displayProducts() {
           name: `Quantity`
         }
       ])
-      //Check if store has enough of the product to meet customers request.
       .then(function (number) {
         let item = number.ID
         let quantity = number.Quantity
-        //Print the customers request
+        //Print the customers purchase request
         console.log(`Requested Item ID: ${item}`)
         console.log(`Requested Quantity: ${quantity}`)
 
-        let selectQuery = `SELECT * FROM products WHERE ?`
-        db.query(selectQuery, { item_id: item }, (e, data) => {
+        //Check if items are in stock
+        db.query(`SELECT * FROM products WHERE ?`, { item_id: item }, (e, data) => {
           if (e) {
             console.log(e)
           }
           //Validate item id data
           if (data.length === 0) {
             console.log(`Please enter a valid Item ID`)
-            displayProducts()
+            console.log(`Products:`)
+            console.log(prodList)
           } else {
             let product = data[0]
 
             //check if product is in stock
             if (quantity <= product.stock_quantity) {
               //update the database with new item quantity
-              let updateData = `UPDATE products SET quantity_stock = ` + (products.stock_quantity - quantity) + `WHERE item_id = ` + item
+              let updateData = `UPDATE products SET quantity_stock = ` + (product.stock_quantity - quantity) + `WHERE item_id = ` + item
 
               db.query(updateData, (e, data) => {
                 if (e) {
                   console.log(e)
                 }
-                console.log(`Your total is $` + products.price * quantity)
+                console.log(`Your total is $` + product.price * quantity)
                 console.log(`Your order has been placed!`)
 
-                db.end()
               })
             } else {
               console.log(`We're sorry, we are low in stock on your item`)
               console.log(`Please modify your order or choose another item.`)
-
-              displayProducts()
             }
           }
         })
@@ -88,4 +87,4 @@ function displayProducts() {
   })
 }
 
-displayProducts()
+bamazon()
